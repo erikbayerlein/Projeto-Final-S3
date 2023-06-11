45a89b0e6babeea3a2fa0bf663aa0fe4cf0cf4c4
Leonan Marques <leonan.marques886@gmail.com>
2023-06-11 00:11:54 -0300

    feature(): product controller class have been created

---
+++ api/src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/product")
+@CrossOrigin
+@RequiredArgsConstructor
+public class ProductController {
+}
