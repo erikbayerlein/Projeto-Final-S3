1bd7de1e21a657916f1fe4ccdda1ff54a751b77c
Leonan Marques <leonan.marques886@gmail.com>
2023-06-20 19:03:02 -0300

    feature(): jwt services have been implemented

--- api/src/main/java/com/example/demo/service/JwtService.java
+++ api/src/main/java/com/example/demo/service/JwtService.java
@@ -2,11 +2,16 @@ package com.example.demo.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -16,6 +21,20 @@ public class JwtService {
 
     public String extractCPF(String token){
         return extractClaim(token, Claims::getSubject);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSigninKey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(0), userDetails);
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
@@ -32,10 +51,21 @@ public class JwtService {
                 .getBody();
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String CPF = extractCPF(token);
+        return CPF.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     private Key getSigninKey() {
         byte[] keyBytes = Decoders.BASE64.decode(FINAL_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
-
-
 }
