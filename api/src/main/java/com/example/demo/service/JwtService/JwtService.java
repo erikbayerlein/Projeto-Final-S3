fb934baa465d04bb62af39c244428cf06efb4e36
Leonan Marques <leonan.marques886@gmail.com>
2023-06-19 21:11:26 -0300

    feature(): jwt authentication filter have been created

---
+++ api/src/main/java/com/example/demo/service/JwtService/JwtService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service.JwtService;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface JwtService {
+    String extractUserName(String token);
+
+    String generateToken(UserDetails userDetails);
+
+    boolean isTokenValid(String token, UserDetails userDetails);
+}
